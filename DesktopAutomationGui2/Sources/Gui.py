41fcd92d8d68f47ce1e468684f1c7bbc1ebc4e39
jpgros <jeanphilippe.gros1@gmail.com>
2022-06-02 15:24:27 +0200

    Added possibility to input keyboard actions Added comments to the methods Todo :   keyboard special commands implems 	 verify/test the app

--- DesktopAutomationGui2/Sources/Gui.py
+++ DesktopAutomationGui2/Sources/Gui.py
@@ -1,3 +1,5 @@
+import pyautogui as pyautogui
+
 import Sources.backEnd as backEnd
 import os
 from tkinter import *
@@ -9,14 +11,37 @@ from functools import partial
 
 #import main
 
-
+import pyautogui
+import time
 class Gui:
     infoMsg="tato"
     msgArea="Please select area before submitting"
     mainFile = None
 
-
+    class Parameters:
+        timeBetweenClick=-1
+        entryNB=-1
+        entryDelay=-1
+        timePressed=-1
+        clickMode=None
+        durationMode=None
+        keyboardEntry=""
+    """
+    First window to appear only basic operation can be done here
+    Add area : not used anymore
+    Quit : close the window
+    Adding command let usef add clicks/keyboard script parametrizing with a new window
+    Load CS : let user load a script already created and open it on a text editor automaticaly.
+    Clear file : let user remove the content of a created file
+    Let's Go : launch a selected script, if no script selected the app ask for a file.
+    """
     def launchWindow(self):
+        i=0
+        while(i<1):
+            print(pyautogui.position())
+            time.sleep(1)
+            i+=1
+
         m_w = tk.Tk()
         frm = ttk.Frame(m_w, padding=10)
 
@@ -28,11 +53,13 @@ class Gui:
         tk.Button(frm, text="Adding commands", command=self.clickingWindows).grid(column=2, row=0)
         tk.Button(frm, text="Load CS", command= lambda : self.selectFile(True)).grid(column=0, row=1)
         tk.Button(frm, text="Clear File", command=self.clearFile).grid(column=1, row=1)
-        tk.Button(frm, text="Let's Go", command=lambda : backEnd.clickProcess(self.mainFile)).grid(column=2, row=1)
+        tk.Button(frm, text="Let's Go", command=lambda : backEnd.clickProcess(self.mainFile,self.Parameters)).grid(column=2, row=1)
 
         #msgGUI.trace('w',my_r)
         tk.mainloop()
-
+    """
+    Remove soon
+    """
     def addAreaWindows(self):
         area = Tk()
         frm2= ttk.Frame(area, padding=10)
@@ -45,7 +72,10 @@ class Gui:
         en.grid(row=2, column=2)
         ttk.Button(frm2, text="submitcoords", command=backEnd.setCoords).grid(column=0, row=3)
         ttk.Button(frm2, text="done", command=area.destroy).grid(column=1, row=3)
-
+    """
+    Clear the content of the file
+    Opens a file explorer and the selected file will be erased
+    """
     def clearFile(self):
         filename = fd.askopenfilename(initialdir=os.getcwd())
         f = open(filename, "w")  # Create a blank file
@@ -53,48 +83,83 @@ class Gui:
         f.close()  # Close file
         print("suppressed successfully")
 
+
+    """
+    Disable the possibility to enter and clear the content writen
+    """
+    def disableEntry(self, entry):
+        entry.delete(0, 'end')
+        entry.config(state="disabled")
+    """
+    Window containing a lots of items for the script parametrizing
+    3 modes ares possibles choosen with radio buttons
+    Multiple clicks wich leads to another choice between 
+    a number of clicks or a duration of clicks
+    Single clicks will proceed to only one click
+    Keyboard touch let user enter keyboard commands,
+    For keyboard input:    
+        the ";" character cut two sequences
+        the "+" character ask to press simultaneously the keyboard keys
+    For every possibilities it is possible to add a delay after click or a keyboard pressed
+    The button Choose file allows to choose a file where the script will be written
+    The button Submit let the user submit each action
+    The button Done closes the window
+    """
     def clickingWindows(self):
+        params = self.Parameters()
         def updateAreaInfo(*args):
             print("called method" + self.msgArea)
             selectedArea.set(self.msgArea)
             print(selectedArea.get())
         def my_r(*args): #args avoid error of passing arguments
-            msgGUI.set("Submited " + str(timeBetweenClick.get()) + " " + str(entryNB) + " " + str(entryDelay) + " " + str(timePressed))
+            msgGUI.set("Submited " + str(self.Parameters.timeBetweenClick.get()) + " " + str(self.Parameters.entryNB.get())
+                       + " " + str(self.Parameters.entryDelay.get()) + " " + str(self.Parameters.timePressed.get()))
             print(msgGUI.get())  # Print when variable changes.
+        """
+        This method manages the element of the window.
+        It update the state of the elements, which guides the user 
+        how to fill the parameters
+        """
         def selection(*args):
-            print("You selected the option " + str(clickMode.get()))
-            if(str(clickMode.get())=="Multiple"):
-                timeBetweenClick.config(state="enabled")
-                entryNB.config(state="enabled")
-                timePressed.config(state="disabled")
-                keyboardEntry.config(state="disabled")
-            if (str(clickMode.get()) == "Single"):
-                timeBetweenClick.config(state="disabled")
-                entryNB.config(state="disabled")
-                timePressed.config(state="enabled")
-                keyboardEntry.config(state="disabled")
-            if(str(clickMode.get()) == "Keyboard"):
-                timeBetweenClick.config(state="disabled")
-                entryNB.config(state="disabled")
-                timePressed.config(state="disabled")
-                keyboardEntry.config(state="enabled")
+            print("You selected the option " + str(self.Parameters.clickMode.get()))
+            if(str(self.Parameters.clickMode.get())=="Multiple"):
+                self.disableEntry(self.Parameters.timePressed)
+                self.disableEntry(self.Parameters.keyboardEntry)
+                self.Parameters.timeBetweenClick.config(state="enabled")
+                self.Parameters.entryNB.config(state="enabled")
+                radioClick.config(state="enabled")
+                radioClick2.config(state="enabled")
+            if (str(self.Parameters.clickMode.get()) == "Single"):
+                self.disableEntry(self.Parameters.timeBetweenClick)
+                self.disableEntry(self.Parameters.entryNB)
+                self.disableEntry(self.Parameters.keyboardEntry)
+                self.Parameters.timePressed.config(state="enabled")
+                radioClick.config(state="disabled")
+                radioClick2.config(state="disabled")
+            if(str(self.Parameters.clickMode.get()) == "Keyboard"):
+                self.disableEntry(self.Parameters.timeBetweenClick)
+                self.disableEntry(self.Parameters.entryNB)
+                self.disableEntry(self.Parameters.timePressed)
+                self.Parameters.keyboardEntry.config(state="enabled")
+                radioClick.config(state="disabled")
+                radioClick2.config(state="disabled")
 
         def fakeCallback(*args):
             print("callback called")
-            print("duration mode " + str(durationMode.get()))
+            print("duration mode " + str(self.Parameters.durationMode.get()))
         clickingWindow= Tk()
         msgGUI = tk.StringVar(clickingWindow)
         msgGUI.set("")
-        clickMode = tk.StringVar(clickingWindow)
+        self.Parameters.clickMode = tk.StringVar(clickingWindow)
         selectedArea = tk.StringVar(clickingWindow)
-        durationMode = tk.StringVar(clickingWindow)
+        self.Parameters.durationMode = tk.StringVar(clickingWindow)
 
         selectedArea.set(self.msgArea)
-        clickMode.set("Multiple Clicks")
+        self.Parameters.clickMode.set("Multiple Clicks")
         #durationMode.set("Clicks")
         #adding trace to bind stringvar to actions trigger by GUI
-        clickMode.trace('w', selection)
-        durationMode.trace('w', fakeCallback)
+        self.Parameters.clickMode.trace('w', selection)
+        self.Parameters.durationMode.trace('w', fakeCallback)
         msgGUI.trace('w', my_r)
         selectedArea.trace('w', updateAreaInfo)
         frm2 = ttk.Frame(clickingWindow, padding=10)
@@ -103,56 +168,56 @@ class Gui:
         ttk.Button(frm2, text="select area", command=lambda:[backEnd.retrievingMouseCoord(4), selectedArea.set(self.msgArea)]).grid(column=1, row=1)
         ttk.Label(frm2, textvariable=selectedArea).grid(row=1, column=2)
 
-        r1 = ttk.Radiobutton(frm2, text='Multiple Clicks', variable=clickMode, value='Multiple', command=lambda: clickMode.set("Multiple Clicks"))
+        r1 = ttk.Radiobutton(frm2, text='Multiple Clicks', variable=self.Parameters.clickMode, value='Multiple', command=lambda: self.Parameters.clickMode.set("Multiple Clicks"))
         r1.grid(row=2,column=1)
         r1['command'] = partial(selection)
-        r2 = ttk.Radiobutton(frm2, text='Single Click' , variable=clickMode, value='Single', command=lambda: clickMode.set("Single Click"))
+        r2 = ttk.Radiobutton(frm2, text='Single Click' , variable=self.Parameters.clickMode, value='Single', command=lambda: self.Parameters.clickMode.set("Single Click"))
         r2.grid(row=2, column=2)
         r2['command'] = partial(selection)
 
-        r3 = ttk.Radiobutton(frm2, text='Keyboard Touch', variable=clickMode, value='Keyboard',
-                             command=lambda: clickMode.set("Keyboard Touch"))
+        r3 = ttk.Radiobutton(frm2, text='Keyboard Touch', variable=self.Parameters.clickMode, value='Keyboard',
+                             command=lambda: self.Parameters.clickMode.set("Keyboard Touch"))
         r3.grid(row=2, column=3)
         r3['command'] = partial(selection)
 
         ttk.Label(frm2, text="Time between click (s)").grid(row=3, column=0)
-        timeBetweenClick = ttk.Entry(frm2)
-        timeBetweenClick.grid(row=3, column=1)
-        timeBetweenClick.config(state="disabled")
+        self.Parameters.timeBetweenClick = ttk.Entry(frm2)
+        self.Parameters.timeBetweenClick.grid(row=3, column=1)
+        self.Parameters.timeBetweenClick.config(state="disabled")
 
         ttk.Label(frm2, text="Delay after click(s)").grid(row=3, column=2)
-        entryDelay = ttk.Entry(frm2)
-        entryDelay.grid(row=3, column=3)
+        self.Parameters.entryDelay = ttk.Entry(frm2)
+        self.Parameters.entryDelay.grid(row=3, column=3)
         ttk.Label(frm2, text="Number of ").grid(row=4, column=0)
 
-        radioClick = ttk.Radiobutton(frm2, text='click (s)', variable=durationMode, value='Clicks',
-                             command=lambda: durationMode.set("Clicks"))
+        radioClick = ttk.Radiobutton(frm2, text='click (s)', variable=self.Parameters.durationMode, value='Clicks',
+                             command=lambda: self.Parameters.durationMode.set("Clicks"))
         radioClick.grid(row=4, column=1)
         radioClick['command'] = partial(fakeCallback)
 
-        radioClick2 = ttk.Radiobutton(frm2, text='second(s) clicking', variable=durationMode, value="Seconds",
-                             command=lambda: durationMode.set("Seconds"))
+        radioClick2 = ttk.Radiobutton(frm2, text='second(s) clicking', variable=self.Parameters.durationMode, value="Seconds",
+                             command=lambda: self.Parameters.durationMode.set("Seconds"))
         radioClick2.grid(row=4, column=2)
         radioClick2['command'] = partial(fakeCallback)
 
-        entryNB = ttk.Entry(frm2)
-        entryNB.grid(row=4, column=3)
-        entryNB.config(state="disabled")
+        self.Parameters.entryNB = ttk.Entry(frm2)
+        self.Parameters.entryNB.grid(row=4, column=3)
+        self.Parameters.entryNB.config(state="disabled")
 
         ttk.Label(frm2, text="Time maintaining click").grid(row=5, column=0)
-        timePressed = ttk.Entry(frm2)
-        timePressed.grid(row=5, column=1)
-        timePressed.config(state="disabled")
+        self.Parameters.timePressed = ttk.Entry(frm2)
+        self.Parameters.timePressed.grid(row=5, column=1)
+        self.Parameters.timePressed.config(state="disabled")
 
         ttk.Label(frm2, text="Keyboard Input").grid(row=5, column=2)
-        keyboardEntry = ttk.Entry(frm2)
-        keyboardEntry.grid(row=5, column=3)
-        keyboardEntry.config(state="disabled")
+        self.Parameters.keyboardEntry = ttk.Entry(frm2)
+        self.Parameters.keyboardEntry.grid(row=5, column=3)
+        self.Parameters.keyboardEntry.config(state="disabled")
 
-        newFileB= ttk.Button(frm2, text="New File",command =lambda : self.selectFile(False))
+        newFileB= ttk.Button(frm2, text="Choose File",command =lambda : self.selectFile(False))
         newFileB.grid(row=6,column=0)
         b1=ttk.Button(frm2, text="Submit", command=lambda
-        :[backEnd.submitClickingScript(timeBetweenClick, entryNB, entryDelay, timePressed, clickMode, durationMode), msgGUI.set("Submited ")])
+        :[backEnd.submitClickingScript(self.Parameters), msgGUI.set("Submited ")])
         b1.grid(row=6,column=1)
         ttk.Button(frm2, text="Done", command=clickingWindow.destroy).grid(row=6,column=2)
 
@@ -160,7 +225,12 @@ class Gui:
         l1.grid(row=7, column=0)
 
         tk.mainloop()
-
+    """
+    Open an explorer window to select a file
+    If bool is true a text editor is opened
+    
+    @bool boolean for opening the text editor
+    """
     def selectFile(self, bool):
         #dirname = ttk.askdirectory()
         self.mainFile = fd.askopenfilename(initialdir = os.getcwd())
--- DesktopAutomationGui2/Sources/backEnd.py
+++ DesktopAutomationGui2/Sources/backEnd.py
@@ -1,116 +1,201 @@
+import inspect
 import time
 import random
+
+import pyautogui
+
 import Sources.Gui as Gui
 import os
 #import main
 from tkinter import filedialog as fd
 from pynput import mouse
-from pynput.mouse import Button, Controller, Listener
+from pynput.mouse import Button, Listener
+from pynput.mouse import Controller as mouseController
+from pynput.keyboard import Controller as keyboardController
+
 class backEnd:
 	coords=[]
 
-inst = backEnd()
-mouse = Controller()
+instBackEnd = backEnd()
+mouseC = mouseController()
+keyboardC = keyboardController()
 strFile= "" #"C:/Users/jgr22027/OneDrive - OPEN/Documents/ScriptFiles/"
+
+"""
+Method to manage keyboard actions depending on macros
+Existing macros 
+";" for sequence of actions
+"+" for a simultaneous key press
+"§cmd§" for running a special command
+For example §ctrl§ will press ctrl key
+"""
+def keyboardActions(keyboard):
+	actions = keyboard.split(";")
+	for elt in actions:
+		print("try to write "+ str(elt))
+		keysPressed = elt.split("+")
+		for key in keysPressed:
+			for char in key:
+				print("try to type " + str(char))
+				keyboardC.press(char)
+				keyboardC.release(char)
+"""
+Returns int average between two parameters
+@a any type accepted but need to be a number for cast to float 
+@b any type accepted but need to be a number for cast to float
+"""
+def moy(a ,b):
+	return int((float(a) + float(b))/2.0)
+
+"""
+Method to manage mouse actions depending on parameters
+@line1 line containing first couple of coords
+@line2 line containing second couple of coords
+@timePressed containing time between click and release mouse bouton
+@entryDelay containing the delay after the click
+"""
+def mouseActions(line1, line2, timePressed, entryDelay):
+	coords1 = line1.split(";")
+	coords2 = line2.split(";")
+
+	print("clicking on " + str(moy(coords1[0],coords2[0])) + " " + str(moy(coords1[1],coords2[1])))
+	mouseC.position =moy(coords1[0],coords2[0]), moy(coords1[1],coords2[1])
+	mouseC.press(Button.left)
+	time.sleep(timePressed)
+	mouseC.release(Button.left)
+	time.sleep(float(entryDelay))
+	print("Current position: " + str(mouseC.position))
+
+"""
+Method retrieving the information for the script in the file
+and calling the corresponding methods
+@file input file containing the informations
+"""
 def clickProcess(file):
+	#print(inspect.currentframe())
 	try:
-		f = open("toto") #open(strFile+"clickingScript.txt","r")
-		print(strFile +"clickingScript.txt","r")
+		f = open(file) #open(strFile+"clickingScript.txt","r")
+		print("given file "+ file)
 	except (FileNotFoundError) as e:
 		file = fd.askopenfilename(initialdir=os.getcwd())
 		f=open(file)
 		print(f)
 	while True:
+		print("while loop")
 		try:
 			timeBetweenClick = float(f.readline())
 			entryNB= int(f.readline())
 			timePressed= float(f.readline())
+			keyboard=f.readline()
 			entryDelay= float(f.readline())
-			line= f.readline()
-			line= f.readline()
-			#if(line.__contains__(';')) : break
-			coords = line.split(";")
-			print(coords[0] + " " + coords[1])
-			mouse.move( int(coords[0]), int(coords[1]))
-			mouse.press(Button.left)
-			time.sleep(timePressed)
-			mouse.release(Button.left)
-			time.sleep(float(entryDelay))
-			print("Current position: " + str(mouse.position))
+			coords1= f.readline()
+			coords2= f.readline()
 		except (AttributeError, ValueError) as e:
-			print("catched "+ str(e))
+			print("catched err reading script files values" + str(e))
+			break
+			#if(line.__contains__(';')) : break
+		try:
+			if entryNB==-1 and timeBetweenClick==-1 and timePressed==-1: # clickmode = keyboard
+				print("keyboard")
+				keyboardActions(keyboard)
+
+			elif entryNB!=-1 or timeBetweenClick!=-1 or timePressed!=-1: #clickmode = single or multiple
+				print("mouse")
+				mouseActions(coords1, coords2,timePressed,entryDelay)
+			else:
+				print("ERROR : clickMode in function " + inspect.currentframe() + " not taken into account")
+
+		except (AttributeError, ValueError) as e:
+			print("catched err using keyboard/mouse script actions" + str(e))
 			break
 
-
 def printSelectedCoord():
-	for x in inst.coords:
+	for x in instBackEnd.coords:
 		print(" coord loop "+ str(x))
 
+"""
+Retrieve an area with 2 clicks
+The listener is called twice
+"""
 def retrievingMouseCoord(nb):
-
-	def on_click(x, y, button, pressed):
-		if pressed:
-			inst.coords.append(x)
-			inst.coords.append(y)
-			print('{0} at {1}'.format(
-				'Pressed' if pressed else 'Released',
-				(x, y)))
-			print("len" + str(len(inst.coords)))
-			if len(inst.coords)>=nb:
-				# Stop listener
-				print(" try ")
-				Gui.msgArea= "Selected (" + str(inst.coords.__getitem__(len(inst.coords) - 4)) + "," + str(inst.coords.__getitem__(len(inst.coords) - 3))
-				Gui.msgArea = Gui.msgArea + ") (" + str(inst.coords.__getitem__(len(inst.coords) - 2)) + "," + str(inst.coords.__getitem__(len(inst.coords) - 1)) + ")"
-				print("strarea should be " + Gui.msgArea)
-				return False
-
 	# Collect events until released
 	with Listener(
 			on_click=on_click) as listener:
 		listener.join()
 
-def verifPos(x,y):
-	if(x > 500 or x <300):
-		x= (random.random()*200)+300
-	if(y > 800 or y <300):
-		y= (random.random()*500)+300
-	mouse.position= (x,y)
-def setCoords():
-	f = open(strFile+"areas.txt", "a")
-	f.write("wrting")
-	f.write(str(inst.coords[0]) +";"+ str(inst.coords[1])+ "\n")
-	inst.coords.clear()
-	f.close()
+	with Listener(
+			on_click=on_click) as listener:
+		listener.join()
+"""
+Bridge between gui interface and backend
+Retrieve click and add it to the list
+"""
+def on_click(x, y, pressed):
+	if pressed:
+		print("coords clicked "  + str(x) + " " + str(y))
+		for i in instBackEnd.coords:
+			print (str(i) + " ")
+		instBackEnd.coords.append(x)
+		instBackEnd.coords.append(y)
+		print('{0} at {1}'.format(
+			'Pressed' if pressed else 'Released',
+			(x, y)))
+		print("len" + str(len(instBackEnd.coords)))
+		if len(instBackEnd.coords)>=2:
+			# Stop listener
+			Gui.msgArea= "Selected (" + str(instBackEnd.coords.__getitem__(len(instBackEnd.coords) - 4)) + "," + str(instBackEnd.coords.__getitem__(len(instBackEnd.coords) - 3))
+			Gui.msgArea = Gui.msgArea + ") (" + str(instBackEnd.coords.__getitem__(len(instBackEnd.coords) - 2)) + "," + str(instBackEnd.coords.__getitem__(len(instBackEnd.coords) - 1)) + ")"
+			print("strarea should be " + Gui.msgArea)
+			return False
+
+# def verifPos(x,y):
+# 	if(x > 500 or x <300):
+# 		x= (random.random()*200)+300
+# 	if(y > 800 or y <300):
+# 		y= (random.random()*500)+300
+# 	mouse.position= (x,y)
+
+# def setCoords():
+# 	f = open(strFile+"areas.txt", "a")
+# 	f.write("wrting")
+# 	f.write(str(instBackEnd.coords[0]) +";"+ str(instBackEnd.coords[1])+ "\n")
+# 	instBackEnd.coords.clear()
+# 	f.close()
 def retrieveClickingScript():
 	f = open(strFile+"clickingScript.txt", "a")
 
-def submitClickingScript(timeBetweenClick, entryNB,entryDelay,timePressed, clickMode, durationMode):
+"""
+Submit an action by writing it on a file
+IF all parameters are well defined all the parameters are writen in the script file
+"""
+def submitClickingScript(parameters):
 	f = open(strFile+"clickingScript.txt", "a")
 	res=""
-	res = timeBetweenClick.get() if timeBetweenClick.get() !=None and clickMode.get()=="Multiple" else "-1"
+	res = parameters.timeBetweenClick.get() if parameters.timeBetweenClick.get() !=None and parameters.clickMode.get()=="Multiple" else "-1"
 	f.write(res+"\n")
-	if entryNB.get() != None and clickMode.get()=="Multiple":
-		if durationMode.get()=="Clicks":
-			res = "C:" + entryNB.get()
-		elif durationMode.get()=="Seconds":
-			res = "S:" + entryNB.get()
+	if parameters.entryNB.get() != None and parameters.clickMode.get()=="Multiple":
+		if parameters.durationMode.get()=="Clicks":
+			res = "C:" + parameters.entryNB.get()
+		elif parameters.durationMode.get()=="Seconds":
+			res = "S:" + parameters.entryNB.get()
 		else:
-			print("Duration mode " + str(durationMode.get()) + "not taken into account")
+			print("Duration mode " + str(parameters.durationMode.get()) + "not taken into account")
 			print("Please be sure you selected either a duration or a click number if you chose multiple clicks")
 	else:
 		res= "-1"
 	f.write(res+"\n")
-	res = timePressed.get() if timePressed.get() != None and clickMode.get()=="Single" else "-1"
+	res = parameters.timePressed.get() if parameters.timePressed.get() != None and parameters.clickMode.get()=="Single" else "-1"
 	f.write(res + "\n")
-	res = entryDelay.get() if entryDelay.get() != None else "0"
+	res = parameters.keyboardEntry.get() if parameters.clickMode.get() == "Keyboard" else ""
+	f.write(res + "\n")
+	res = parameters.entryDelay.get() if parameters.entryDelay.get() != None else "0"
 	f.write(res + "\n")
 
-	f.write(str(inst.coords[0]) +";"+ str(inst.coords[1])+ "\n")
-	f.write(str(inst.coords[2]) +";"+ str(inst.coords[3])+ "\n")
-	inst.coords.clear()
+	f.write(str(instBackEnd.coords[0]) +";"+ str(instBackEnd.coords[1])+ "\n")
+	f.write(str(instBackEnd.coords[2]) +";"+ str(instBackEnd.coords[3])+ "\n")
+	instBackEnd.coords.clear()
 	f.close()
 	Gui.infoMsg= "Writen done"
 
-print ("Current positions: " + str(mouse.position))
 #mouse.position = (404, 879)#absolute
 
